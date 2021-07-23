8bdb9b78970a15e852334d7a7bb3329c4f61ddf5
Albert <albertraphel@gmail.com>
2021-07-23 16:13:37 +0530

    initial commit

---
+++ src/main/java/com/albert/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,20 @@
+package com.albert.microservices.currencyconversionservice.proxy;
+
+import com.albert.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from,
+                                                @PathVariable String to);
+
+}
